e88cabd635ab6924b69142b02abbcb710d784c46
YaninaBrizg <yanina.brizg@gmail.com>
2023-12-16 22:26:23 +0100

    homework-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,15 @@
-const banWord1 = `spam`;
-const banWord2 = `sale`;
-function checkForSpam(message) {
-  return (
-    message.toLowerCase().includes(banWord1) ||
-    message.toLowerCase().includes(banWord2)
-  );
+function filterArray(numbers, value) {
+  let filteredNumbers = [];
+  for (let elem of numbers) {
+    if (elem > value) {
+      filteredNumbers.push(elem);
+    }
+  }
+  return filteredNumbers;
 }
 
-console.log(checkForSpam("Latest technology news"));
-console.log(checkForSpam("JavaScript weekly newsletter"));
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Amazing SalE, only tonight!"));
-console.log(checkForSpam("Trust me, this is not a spam message"));
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
-console.log(checkForSpam("[SPAM] How to earn fast money?"));
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
